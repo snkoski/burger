7c2c1ca0909a1b8028a6276893034a122b86080f
Jeremy Gill <jeremygill87@gmail.com>
2019-12-18 15:24:50 -0500

    Coded SQL database and Seeds values

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("double cheeseburger", false),
+("black bean burger", false),
+("fried chicken sandwich", false)
+("stanky breath burger", false)
