d90b6c54ec67d63a0e9d9db308df75a0107085c1
Jeremy Gill <jeremygill87@gmail.com>
2019-12-19 11:18:57 -0500

    set up controller files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,43 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devourd
+    ], function (result) {
+        res.json({  id: result.insertId});
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result){
+        if (result.changedRows == 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
