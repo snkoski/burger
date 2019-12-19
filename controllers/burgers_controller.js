5d81dd053659197fd6c85781da38e30e13f615fc
Jeremy Gill <jeremygill87@gmail.com>
2019-12-19 11:39:59 -0500

    Testing and pushing through basic framework

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 router.get("/", function(req, res){
     burger.selectAll(function(data){
         var hbsObject = {
-            burger: data
+            burgers: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
 app.listen(PORT, function() {
     console.log("Server listening on http://localhost:" + PORT);
 })
