cd4d1814eebe4d31053ab964ff519730f5368f13
Jeremy Gill <jeremygill87@gmail.com>
2019-12-20 09:56:38 -0500

    added package.json file

--- config/connection.js
+++ config/connection.js
@@ -8,15 +8,9 @@ if (process.env.JAWSDB_URL) {
         user: "root",
         password: "Piggy666!",
         database: "burgers_db"
-    })
-}
+    });
+};
 
-connection.connect(function(err){
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
