1f0e66508ba8d3d449920cc184bca7353d1ba07d
Jeremy Gill <jeremygill87@gmail.com>
2019-12-26 17:44:39 -0500

    attempted to fix Heroku functionality, unsuccessfully

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,19 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: "localhost",
+        port: 3306, 
         user: "root",
         password: "Piggy666!",
         database: "burgers_db"
     });
 };
 
-connection.connect();
+connection.connect(function(err){
+    if (err) {
+        console.error("error connecting: " = err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
